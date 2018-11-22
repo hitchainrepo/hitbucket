1e81d5fd47a0415f34ee067326df17891ecb900c
tylerchen <iffiff1@gmail.com>
2018-11-22 09:56:53 +0800

    add api by tylerchen

---
+++ src/main/java/org/hitchain/hit/api/ObjectAccess.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2018-11-21 @author <a href="mailto:iffiff1@gmail.com">Tyler Chen</a>.
+ * All rights reserved.
+ *
+ * Contributors:
+ *     <a href="mailto:iffiff1@gmail.com">Tyler Chen</a> - initial API and implementation.
+ * Auto Generate By foreveross.com Quick Deliver Platform. 
+ ******************************************************************************/
+package org.hitchain.hit.api;
+
+/**
+ * ObjectAccess
+ *
+ * @author <a href="mailto:iffiff1@gmail.com">Tyler Chen</a>
+ * @since 2018-11-21
+ * auto generate by qdp.
+ */
+public interface ObjectAccess {
+
+}
