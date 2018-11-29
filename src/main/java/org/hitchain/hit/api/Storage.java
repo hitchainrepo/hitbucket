c9fad527610b0afd9d229282d4c790c96df37b06
tylerchen <iffiff1@gmail.com>
2018-11-29 14:25:43 +0800

    add file by tylerchen

---
+++ src/main/java/org/hitchain/hit/api/Storage.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018-11-22 @author <a href="mailto:iffiff1@gmail.com">Tyler Chen</a>.
+ * All rights reserved.
+ *
+ * Contributors:
+ *     <a href="mailto:iffiff1@gmail.com">Tyler Chen</a> - initial API and implementation.
+ * Auto Generate By foreveross.com Quick Deliver Platform. 
+ ******************************************************************************/
+package org.hitchain.hit.api;
+
+/**
+ * Storage
+ *
+ * @author <a href="mailto:iffiff1@gmail.com">Tyler Chen</a>
+ * @since 2018-11-22
+ * auto generate by qdp.
+ */
+public interface Storage {
+}
