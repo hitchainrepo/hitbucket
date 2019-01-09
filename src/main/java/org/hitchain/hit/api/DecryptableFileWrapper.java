714006d3b4ea5dcbe904be77f631694d0159de14
tylerchen <iffiff1@gmail.com>
2019-01-09 16:39:03 +0800

    change the ecc decrypt repository private key to rsa

--- src/main/java/org/hitchain/hit/api/DecryptableFileWrapper.java
+++ src/main/java/org/hitchain/hit/api/DecryptableFileWrapper.java
@@ -8,12 +8,6 @@
  ******************************************************************************/
 package org.hitchain.hit.api;
 
-import io.ipfs.api.NamedStreamable;
-
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang3.StringUtils;
-import org.hitchain.hit.util.ECCUtil;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,86 +18,95 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hitchain.hit.util.ECCUtil;
+import org.hitchain.hit.util.RSAUtil;
+
+import io.ipfs.api.NamedStreamable;
+
 /**
  * MyFileWrapper
  *
  * @author <a href="mailto:iffiff1@gmail.com">Tyler Chen</a>
- * @since 2018-11-29
- * auto generate by qdp.
+ * @since 2018-11-29 auto generate by qdp.
  */
 public class DecryptableFileWrapper implements NamedStreamable {
-    private final HashedFile source;
-    private final IndexFile indexFile;
-    private final String account;
-    private final String privateKey;
+	private final HashedFile source;
+	private final IndexFile indexFile;
+	private final String account;
+	private final String privateKey;
 
-    public DecryptableFileWrapper(HashedFile source, IndexFile indexFile, String account, String privateKey) {
-        if (source == null) {
-            throw new IllegalStateException("DecryptableFileWrapper HashedFile does not exist: " + source);
-        } else {
-            this.source = source;
-        }
-        if (indexFile == null) {
-            throw new IllegalStateException("DecryptableFileWrapper IndexFile does not exist: " + indexFile);
-        } else {
-            this.indexFile = indexFile;
-        }
-        if (indexFile.isPrivate() && StringUtils.isBlank(account)) {
-            throw new IllegalStateException("DecryptableFileWrapper account does not exist: " + account);
-        } else {
-            this.account = account;
-        }
-        if (indexFile.isPrivate() && StringUtils.isBlank(privateKey)) {
-            throw new IllegalStateException("DecryptableFileWrapper privateKey does not exist: " + privateKey);
-        } else {
-            this.privateKey = privateKey;
-        }
-    }
+	public DecryptableFileWrapper(HashedFile source, IndexFile indexFile, String account, String privateKey) {
+		if (source == null) {
+			throw new IllegalStateException("DecryptableFileWrapper HashedFile does not exist: " + source);
+		} else {
+			this.source = source;
+		}
+		if (indexFile == null) {
+			throw new IllegalStateException("DecryptableFileWrapper IndexFile does not exist: " + indexFile);
+		} else {
+			this.indexFile = indexFile;
+		}
+		if (indexFile.isPrivate() && StringUtils.isBlank(account)) {
+			throw new IllegalStateException("DecryptableFileWrapper account does not exist: " + account);
+		} else {
+			this.account = account;
+		}
+		if (indexFile.isPrivate() && StringUtils.isBlank(privateKey)) {
+			throw new IllegalStateException("DecryptableFileWrapper privateKey does not exist: " + privateKey);
+		} else {
+			this.privateKey = privateKey;
+		}
+	}
 
-    public InputStream getInputStream() throws IOException {
-        if (indexFile.isPrivate()) {
-            String encrypt = null;
-            if (account.equals(indexFile.getOwner()) || account.equals(indexFile.getOwnerPublicKey())) {
-                encrypt = indexFile.getRepositoryPrivateKeyEncrypted();
-            } else if (indexFile.getMemberKeys().containsKey(account)) {
-                encrypt = indexFile.getMemberRepositoryKeys().get(indexFile.getMemberKeys().get(account));
-            } else if (indexFile.getMemberRepositoryKeys().containsKey(account)) {
-                encrypt = indexFile.getMemberRepositoryKeys().get(account);
-            }
-            try {
-                byte[] repositoryPrivateKey = ECCUtil.privateDecrypt(Hex.decodeHex(encrypt.toCharArray()), ECCUtil.getPrivateKeyFromEthereumHex(privateKey));
-                InputStream is = source.getInputStream();
-                byte[] bytes = ECCUtil.privateDecrypt(is, ECCUtil.getPrivateKeyFromEthereumHex(new String(repositoryPrivateKey)));
-                is.close();
-                return new ByteArrayInputStream(bytes);
-            } catch (Exception e) {
-                throw new IOException(e);
-            }
-        }
-        return source.getInputStream();
-    }
+	public InputStream getInputStream() throws IOException {
+		if (indexFile.isPrivate()) {
+			String encrypt = null;
+			if (account.equals(indexFile.getOwner()) || account.equals(indexFile.getOwnerPublicKey())) {// #if is owner, get the repository encrypted private key.
+				encrypt = indexFile.getRepositoryPrivateKeyEncrypted();
+			} else if (indexFile.getMemberKeys().containsKey(account)) {// #if is member account, get the member repository encrypted private key.
+				encrypt = indexFile.getMemberRepositoryKeys().get(indexFile.getMemberKeys().get(account));
+			} else if (indexFile.getMemberRepositoryKeys().containsKey(account)) {// #if account is public key, get the member repository encrypted private key.
+				encrypt = indexFile.getMemberRepositoryKeys().get(account);
+			}
+			try {
+				if (encrypt == null) {
+					return source.getInputStream();
+				}
+				byte[] repositoryPrivateKey = RSAUtil.decrypt(encrypt.getBytes(), RSAUtil.getPrivateKeyFromBase64(privateKey));
+				InputStream is = source.getInputStream();
+				byte[] bytes = ECCUtil.privateDecrypt(is,
+						ECCUtil.getPrivateKeyFromEthereumHex(new String(repositoryPrivateKey)));
+				is.close();
+				return new ByteArrayInputStream(bytes);
+			} catch (Exception e) {
+				throw new IOException(e);
+			}
+		}
+		return source.getInputStream();
+	}
 
-    public boolean isDirectory() {
-        return this.source.isDirectory();
-    }
+	public boolean isDirectory() {
+		return this.source.isDirectory();
+	}
 
-    public List<NamedStreamable> getChildren() {
-        if (source.isDirectory()) {
-            List<HashedFile> children = source.getChildren();
-            List<NamedStreamable> list = new ArrayList<>();
-            for (HashedFile hf : children) {
-                list.add(new DecryptableFileWrapper(hf, indexFile, account, privateKey));
-            }
-            return list;
-        }
-        return Collections.emptyList();
-    }
+	public List<NamedStreamable> getChildren() {
+		if (source.isDirectory()) {
+			List<HashedFile> children = source.getChildren();
+			List<NamedStreamable> list = new ArrayList<>();
+			for (HashedFile hf : children) {
+				list.add(new DecryptableFileWrapper(hf, indexFile, account, privateKey));
+			}
+			return list;
+		}
+		return Collections.emptyList();
+	}
 
-    public Optional<String> getName() {
-        try {
-            return Optional.of(URLEncoder.encode(this.source.getName(), "UTF-8"));
-        } catch (UnsupportedEncodingException var2) {
-            throw new RuntimeException(var2);
-        }
-    }
+	public Optional<String> getName() {
+		try {
+			return Optional.of(URLEncoder.encode(this.source.getName(), "UTF-8"));
+		} catch (UnsupportedEncodingException var2) {
+			throw new RuntimeException(var2);
+		}
+	}
 }
